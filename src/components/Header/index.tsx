34507521cf70e09074823b85392231c696b816bb
Dan Tovbein <dtovbein@gmail.com>
2020-05-27 20:54:48 -0300

    Implement Header and SASS config

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logoSmall from "../../media/images/logo-small.svg";
+import logo from "../../media/images/logo.svg";
+import "./styles.scss";
+
+
+export const Header: React.FunctionComponent<{}> = () => {
+  return (
+    <header className="header">
+      {/* <img className="header__logo--small" alt="Meema" src={logoSmall}/> */}
+      <img className="header__logo" alt="Meema" src={logo}/>
+    </header>
+  )
+} 
---
+++ src/components/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from "../Header";
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
